932408c88a9f0060138d41bd922932d54889603c
sanhda <maivansanh1993@gmail.com>
2024-06-06 16:46:32 +0700

    UserController 2

--- bai_case_module3/src/main/java/org/example/bai_case_module3/controller/UserController.java
+++ bai_case_module3/src/main/java/org/example/bai_case_module3/controller/UserController.java
@@ -1,4 +1,37 @@
 package org.example.bai_case_module3.controller;
 
-public class UserController {
+import org.example.bai_case_module3.service.UserService;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "UserServlet", urlPatterns = "/user/*")
+public class UserController  extends HttpServlet {
+
+    private UserService userService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.userService = new UserService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = req.getPathInfo();
+        switch (url) {
+            case "/list":
+                try {
+                    this.userService.renderPageListUser(req, resp);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+        }
+    }
 }
--- bai_case_module3/src/main/java/org/example/bai_case_module3/service/UserService.java
+++ bai_case_module3/src/main/java/org/example/bai_case_module3/service/UserService.java
@@ -1,4 +1,25 @@
 package org.example.bai_case_module3.service;
 
+import org.example.bai_case_module3.entity.User;
+import org.example.bai_case_module3.model.user.UserDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 public class UserService {
+
+    private UserDAO userDAO;
+
+    public void renderPageListUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+//        List<User> users = this.studentModel.getAllStudent();
+//        List<Group> groups = this.groupModel.getAllGroup();
+//        request.setAttribute("students", students);
+//        request.setAttribute("groups", groups);
+//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/students/list.jsp");
+//        requestDispatcher.forward(request,response);
+    }
 }
