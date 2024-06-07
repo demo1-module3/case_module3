db1e7dbb51cd8d51e8fe289cdd49149d8505b615
HuuTri331 <nguyenhuutri331@gmail.com>
2024-06-07 15:04:23 +0700

    update

--- bai_case_module3/src/main/java/org/example/bai_case_module3/service/VerityMoneyService.java
+++ bai_case_module3/src/main/java/org/example/bai_case_module3/service/VerityMoneyService.java
@@ -1,4 +1,31 @@
 package org.example.bai_case_module3.service;
 
+import org.example.bai_case_module3.entity.CategoryProduct;
+import org.example.bai_case_module3.entity.VerityMoney;
+import org.example.bai_case_module3.model.verityMoney.VerityMoneyDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 public class VerityMoneyService {
+    private VerityMoneyDAO verityMoneyDAO;
+
+    public VerityMoneyService() {
+        this.verityMoneyDAO = new VerityMoneyDAO();
+    }
+
+    public void renderPageListBook(HttpServletRequest request,
+                                   HttpServletResponse response) throws ServletException, IOException, SQLException {
+        // goi model de lay data
+        List<VerityMoney> verityMoneys = this.verityMoneyDAO.getAllVerMoney();
+        // gan data vao requet de gui xuong view
+        request.setAttribute("verityMoneys", verityMoneys);
+        RequestDispatcher out = request.getRequestDispatcher("/views/categoryView/list.jsp");
+        out.forward(request, response);
+    }
 }
