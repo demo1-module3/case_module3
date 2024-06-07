5c207ed8a475d821971384425d22a262fc9b6047
HuuTri331 <nguyenhuutri331@gmail.com>
2024-06-07 09:35:40 +0700

    update

--- bai_case_module3/src/main/java/org/example/bai_case_module3/controller/SupplierController.java
+++ bai_case_module3/src/main/java/org/example/bai_case_module3/controller/SupplierController.java
@@ -1,4 +1,28 @@
 package org.example.bai_case_module3.controller;
 
-public class SupplierController {
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SupplierController", urlPatterns = "/supplier/*")
+public class SupplierController extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
--- bai_case_module3/src/main/java/org/example/bai_case_module3/service/CategoryService.java
+++ bai_case_module3/src/main/java/org/example/bai_case_module3/service/CategoryService.java
@@ -1,4 +1,30 @@
 package org.example.bai_case_module3.service;
 
+import org.example.bai_case_module3.entity.CategoryProduct;
+import org.example.bai_case_module3.model.categoryProduct.CategoryDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 public class CategoryService {
+    private CategoryDAO categoryDAO;
+
+    public CategoryService() {
+        this.categoryDAO = new CategoryDAO();
+    }
+
+    public void renderPageListBook(HttpServletRequest request,
+                                   HttpServletResponse response) throws ServletException, IOException, SQLException {
+        // goi model de lay data
+        List<CategoryProduct> categories = this.categoryDAO.getAllCategory();
+        // gan data vao requet de gui xuong view
+        request.setAttribute("categories", categories);
+        RequestDispatcher out = request.getRequestDispatcher("/view/categoryeView/list.jsp");
+        out.forward(request, response);
+    }
 }
